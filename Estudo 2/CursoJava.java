6c58e241023f98285754d3a303e21ad5827fd751
Henrique Sampaio Lira <84481479+henriquesampaiolira@users.noreply.github.com>
2022-03-30 00:00:28 -0300

    Utilizando diferentes tipos de prints

---
+++ Estudo 2/CursoJava.java
@@ -0,0 +1,11 @@
+public class CursoJava{
+    public static void main(String[] args){
+
+        int num=865;
+
+        System.out.println("Pulando linha");
+        System.out.print("Curso Java");
+        System.out.printf("%n%s - %d - %f","String para %s",2022,50.50);
+        System.out.printf("%nNúmero digitado é: %d",num);
+    }
+}
