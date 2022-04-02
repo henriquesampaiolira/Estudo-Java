61c5f52ded05fae55974c29ca5989d5e514b89b8
Henrique Sampaio Lira <84481479+henriquesampaiolira@users.noreply.github.com>
2022-04-02 12:10:56 -0300

    Utilizando métodos para trabalhar com arrays

---
+++ Estudo 7/Arrei.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Arrei{
+    public static void main(String[] args) {
+        int[] num={8,5,10,50,3,2,0,9,11};
+        int[] num2=new int[9];
+        int p=0;
+        int pos;
+
+        /*
+    ------------------------------------------------------------------------------------------------
+        Arrays.sort(num); - Método que ordena por ordem de crescimento os valores
+    ------------------------------------------------------------------------------------------------
+        Arrays.fill(num,5); - Método que insere um valor para todos os campos do array
+    ------------------------------------------------------------------------------------------------
+        System.arraycopy(num, 0, num2, 0, num.length); - Método que copia um array para o outro.
+    ------------------------------------------------------------------------------------------------
+        Arrays.equals - Compara arrays e retorna true ou false
+        Ex: 
+        System.out.printf("Os arrays são iguais: %s", Arrays.equals(num, num2) ? "Sim" : "Não");
+    ------------------------------------------------------------------------------------------------
+        Forma de imprimir dados que estão dentro de um array:
+        for(int n:num2){
+            System.out.printf("%d - ",n);
+        }
+    ------------------------------------------------------------------------------------------------
+        Identificar se algum valor está dentro do array, retorna a posição. 
+        OBS: É necessário usar o método sort antes de usar o binarySearch
+
+        pos=Arrays.binarySearch(num, p);
+        System.out.printf("O valor %d está no Array? %s",p, pos > 0 ? "Sim" : "Não");
+    ------------------------------------------------------------------------------------------------
+        */
+        
+
+        
+
+        
+    }
+}
