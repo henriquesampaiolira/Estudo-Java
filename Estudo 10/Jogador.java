c154a75a38a8d2bde2fdc4899ce36d7b778d9385
Henrique Sampaio Lira <84481479+henriquesampaiolira@users.noreply.github.com>
2022-04-06 23:09:52 -0300

    Entendo sobre classes

---
+++ Estudo 10/Jogador.java
@@ -0,0 +1,7 @@
+public class Jogador {
+ 
+    public Jogador(int n){
+        System.out.printf("%nJogador %d criado%n",n);
+    }
+
+}
---
+++ Estudo 10/classes.java
@@ -0,0 +1,9 @@
+public class classes{
+    public static void main(String[] args) {
+        
+        //instanciando novo objeto de uma classe
+        Jogador j1 = new Jogador(1);
+        Jogador j2 = new Jogador(2);
+        Jogador j3 = new Jogador(3);
+    }
+}
