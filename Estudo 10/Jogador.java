39f896cea84aa163f4d154b71f9052623e877557
Henrique Sampaio Lira <84481479+henriquesampaiolira@users.noreply.github.com>
2022-04-06 23:36:29 -0300

    A diferença entre public e private

--- Estudo 10/Jogador.java
+++ Estudo 10/Jogador.java
@@ -1,6 +1,12 @@
 public class Jogador {
- 
+
+    //private só pode ser utilizado dentro da classe, já o public pode ser acessado por fora da classe
+    public int num2=5;
+    private int n=0;
+
     public Jogador(int n){
+        //ao utilizar o "this", ele refere a variável ao que foi criado pela classe anteriormente
+        this.n=n;
         System.out.printf("%nJogador %d criado%n",n);
     }
 
--- Estudo 10/classes.java
+++ Estudo 10/classes.java
@@ -1,9 +1,21 @@
 public class classes{
     public static void main(String[] args) {
+
+        int n=0;
         
-        //instanciando novo objeto de uma classe
-        Jogador j1 = new Jogador(1);
-        Jogador j2 = new Jogador(2);
-        Jogador j3 = new Jogador(3);
+        //instanciando novo objeto de uma classe e usando pré incremento
+        Jogador j1 = new Jogador(++n);
+        Jogador j2 = new Jogador(++n);
+        Jogador j3 = new Jogador(++n);
+
+        //é observado que dependendo de se a variável foi criada com public ou private não dá para acessar ela
+        System.out.println(j1.num);
+        System.out.println(j1.num2);
+
+        //utilizando incremento
+        n=0;
+        Jogador j4 = new Jogador(n++);
+        Jogador j5 = new Jogador(n++);
+        Jogador j6 = new Jogador(n++);
     }
 }
